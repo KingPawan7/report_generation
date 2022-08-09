7a0cfe01e2d73cb53a9da9a66496c77c5c3e5e95
KingPawan7 <shrestha100.pawan@gmail.com>
2022-08-09 15:19:54 +0545

    push to repo

---
+++ include/script.js
@@ -0,0 +1,9 @@
+// let colors=['blue','red', 'yellow', 'pink', 'orange'];
+// let button = document.getElementById('button');
+
+
+// button.addEventListener('click', function(){
+// var randomcolor = colors[Math.floor(Math.random() * colors.length)]
+//  let button = document.getElementById('button');
+//  container.style.button = randomcolor;
+// })
